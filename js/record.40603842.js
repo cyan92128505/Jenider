4485182755df07dcae9d0ab4beba365c956089f3
cyan92128505 <cyan92128505@gmail.com>
2021-01-26 03:15:43 +0000

    Deploying to gh-pages from  @ affef47b5768c5898eb06a993cf0d7ec2d5c49b5 🚀

---
+++ js/record.40603842.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["record"],{"43ef":function(e,c,n){"use strict";n.r(c);var r=n("7a23"),o={class:"record"},s=Object(r["m"])("h1",null,"This is an record page",-1);function t(e,c){return Object(r["D"])(),Object(r["i"])("div",o,[s])}const i={};i.render=t;c["default"]=i}}]);
+//# sourceMappingURL=record.40603842.js.map
