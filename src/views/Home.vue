a5cd1624e323def4e4c667e821322670a077cf3d
cyan92128505 <cyan92128505@gmail.com>
2021-01-26 13:59:07 +0800

    :sparkles: Add component - FateInput

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
 <div class="common-layout">
+  <fate-input></fate-input>
 </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import FateInput from '@/components/FateInput.vue';
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    FateInput,
+  },
 });
 </script>
