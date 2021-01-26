a98f7f33d59b4b824b2ca7cb337d863533caa387
cyan92128505 <cyan92128505@gmail.com>
2021-01-26 13:58:15 +0800

    :lipstick: Modify APP layout

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,27 @@
       </div>
     </el-header>
     <el-main>
-      <router-view />
+      <el-row>
+        <el-col :sm={span:12,offset:6} :xs={span:24}>
+          <router-view />
+        </el-col>
+      </el-row>
     </el-main>
     <el-footer>Footer</el-footer>
   </el-container>
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
@@ -36,6 +45,13 @@
   }
 }
 
+.el-container {
+  display: flex;
+  min-height: 100vh;
+  align-items: stretch;
+  align-content: stretch;
+}
+
 .el-header,
 .el-footer {
   background-color: #b3c0d1;
@@ -43,5 +59,8 @@
 
 .el-main {
   background-color: #e9eef3;
+  flex-grow: 2;
+  flex-shrink: 1;
+  flex-basis: 0%;
 }
 </style>
